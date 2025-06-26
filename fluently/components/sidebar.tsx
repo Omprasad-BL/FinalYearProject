7b691cb43aca1317293250cd7f9ab2615e3641c5
Omprasadbl <omprasadbl@gmail.com>
2025-06-27 00:27:58 +0530

    added all elements of sidebar and account handling

--- fluently/components/sidebar.tsx
+++ fluently/components/sidebar.tsx
@@ -2,6 +2,8 @@ import {cn } from "@/lib/utils"
 import Link  from "next/link";
 import Image from "next/image";
 import { SidebarItem } from "./sidebar-item";
+import { ClerkLoading, ClerkLoaded, UserButton } from "@clerk/nextjs";
+import { Loader } from "lucide-react";
 type Props={
     className?:string;
 };
@@ -18,6 +20,18 @@ export const Sidebar=({className}:Props)=>{
             </Link>
             <div className="flex flex-col gap-y-2 flex-1 ">
                 <SidebarItem label="Learn" href="/learn" iconSrc="/learn.svg"/>
+                <SidebarItem label="Leaderboard" href="/leaderboard" iconSrc="/leaderboard.svg"/>
+                <SidebarItem label="Quests" href="/quests" iconSrc="/quests.svg"/>
+                <SidebarItem label="Shop" href="/shop" iconSrc="/shop.svg"/>
+
+            </div>
+            <div className="p-4">
+                <ClerkLoading>
+                    <Loader className="h-5 w-5 text-muted-foreground animate-spin"/>
+                </ClerkLoading>
+                <ClerkLoaded>
+                    <UserButton afterSignOutUrl="/"/>
+                </ClerkLoaded>
             </div>
         </div>
     )
