01f5068eda72fc443b050f37850a792f6f1e2faf
Omprasadbl <omprasadbl@gmail.com>
2025-07-04 00:37:48 +0530

    Unit and lesson UI added

--- fluently/app/(main)/learn/unit.tsx
+++ fluently/app/(main)/learn/unit.tsx
@@ -36,7 +36,7 @@ export const Unit = ({
   />
     <div className="flex items-center flex-col relative">
         {lessons.map((lesson,index)=>{
-            const isCurrent=lesson.id===activeLesson?.id;
+            const isCurrent=true|| lesson.id===activeLesson?.id;  //remove true later
             const isLocked=!lesson.completed&&!isCurrent;
 
             return (
@@ -45,7 +45,7 @@ export const Unit = ({
                 id={lesson.id}
                 index={index}
                 totalCount={lessons.length-1}
-                current={isCurrent}
+                current={isCurrent} 
                 locked={isLocked}
                 percentage={activeLessonPercentage}
                 />
